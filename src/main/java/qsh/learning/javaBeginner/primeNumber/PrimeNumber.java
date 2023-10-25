e6edc1156f56ca70b4b25c96aa784626a81eb918
Morgan Renou <morgan.renou@4sh.fr>
2023-10-25 15:20:55 +0200

    empty PrimeNumber class

---
+++ src/main/java/qsh/learning/javaBeginner/primeNumber/PrimeNumber.java
@@ -0,0 +1,13 @@
+package qsh.learning.javaBeginner.primeNumber;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(2);
+        System.out.println(3);
+        // ...
+        System.out.println(523);
+        System.out.println(541);
+    }
+
+}
