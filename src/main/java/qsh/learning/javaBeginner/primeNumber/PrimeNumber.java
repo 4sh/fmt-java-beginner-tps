5e332ac0ac2e8d5a69df6c61a0e2173444a5d1ca
Morgan Renou <morgan.renou@4sh.fr>
2023-10-25 15:22:26 +0200

    implements PrimeNumber class

--- src/main/java/qsh/learning/javaBeginner/primeNumber/PrimeNumber.java
+++ src/main/java/qsh/learning/javaBeginner/primeNumber/PrimeNumber.java
@@ -3,11 +3,26 @@ package qsh.learning.javaBeginner.primeNumber;
 public class PrimeNumber {
 
     public static void main(String[] args) {
-        System.out.println(2);
-        System.out.println(3);
-        // ...
-        System.out.println(523);
-        System.out.println(541);
-    }
+        byte nbOfPrimeNumberToFind = 100;
+        byte counter = 0;
 
+        short number = 2; //0, 1 are not prime number by definition.
+
+        while (counter < nbOfPrimeNumberToFind) {
+            boolean isPrimeNumber = true;
+
+            for (short i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    isPrimeNumber = false;
+                }
+            }
+
+            if (isPrimeNumber) {
+                counter++;
+                System.out.println(number);
+            }
+
+            number++;
+        }
+    }
 }
