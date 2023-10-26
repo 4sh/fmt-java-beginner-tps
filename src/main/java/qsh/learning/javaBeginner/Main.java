50d3fdcb90b50aed7ddafddb2eb1b35e708bb717
Morgan Renou <morgan.renou@4sh.fr>
2023-10-26 13:46:21 +0200

    expect different wheel diameter

--- src/main/java/qsh/learning/javaBeginner/Main.java
+++ src/main/java/qsh/learning/javaBeginner/Main.java
@@ -9,12 +9,21 @@ public class Main {
 
         var wheel = new Wheel();
         wheel.brandWheel = "Michelin";
+        /* modifier la taille du pneu à 18, exécuter main */
         wheel.diameter = 19;
 
         car.rightFrontWheel = wheel;
         car.leftFrontWheel = wheel;
         car.rightRearWheel = wheel;
         car.leftRearWheel = wheel;
+
+        /* modifier la taille des pneus à 18 uniquement pour les pneus arrières */
+
+        System.out.println("Wheels checkpoint");
+        System.out.println("Wheel right front should be Michelin/19 : " + car.rightFrontWheel.brandWheel + "/" + car.rightFrontWheel.diameter);
+        System.out.println("Wheel left front Michelin/19 : " + car.leftFrontWheel.brandWheel + "/" + car.leftFrontWheel.diameter);
+        System.out.println("Wheel right rear Michelin/18 : " + car.rightRearWheel.brandWheel + "/" + car.rightRearWheel.diameter);
+        System.out.println("Wheel left rear Michelin/18 : " + car.leftRearWheel.brandWheel + "/" + car.leftRearWheel.diameter);
 
         System.out.println("Car is started " + car.started + " should be false");
         car.started = true;
