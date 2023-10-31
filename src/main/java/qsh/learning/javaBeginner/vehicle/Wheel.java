a1e834706b0b6aafbebc108c32ec862f6617f5e8
Morgan Renou <morgan.renou@4sh.fr>
2023-10-31 21:46:01 +0100

    enum

--- src/main/java/qsh/learning/javaBeginner/vehicle/Wheel.java
+++ src/main/java/qsh/learning/javaBeginner/vehicle/Wheel.java
@@ -2,34 +2,33 @@ package qsh.learning.javaBeginner.vehicle;
 
 import java.util.Objects;
 
-public record Wheel(String brandWheel, int diameter) {
+public record Wheel(BrandWheel brandWheel, int diameter) {
     public static Integer wheelCounter = 0;
 
-    public Wheel {
+    // Enum BrandWheel
+
+   // Enum ypeWheel
+
+    public Wheel(String brandWheel, int diameter) {
+        this(buildBrand(brandWheel), diameter);
         wheelCounter++;
+    }
+
+    public boolean compatibleWith(Wheel wheel) {
+        return Objects.equals(getTypeWheel(), wheel.getTypeWheel());
+    }
+
+    static private BrandWheel buildBrand(String brandWheel) {
+        var brandTag = brandWheel.substring(0, 5).toUpperCase();
+        // construire BrandWheel avec .valueOf()
+    }
+
+    private TypeWheel getTypeWheel() {
+        return brandWheel != null ? brandWheel.type : null;
     }
 
     @Override
     public String toString() {
-        return "R" + diameter + "/" + buildBrandTag(brandWheel);
-    }
-
-    private String buildBrandTag(String brandWheel) {
-        return brandWheel.substring(0, 5).toUpperCase();
-    }
-
-    public boolean compatibleWith(Wheel wheel) {
-        var typeA = getType(brandWheel);
-        var typeB = getType(wheel.brandWheel);
-        return Objects.equals(typeA, typeB);
-    }
-
-    private String getType(String brandWheel) {
-        return switch (buildBrandTag(brandWheel)) {
-            case "MICHE", "GOODY", "HANKO" -> "A";
-            case "CONTI", "PIREL" -> "B";
-            case "DUNLO", "BRIDG" -> "C";
-            default -> null;
-        };
+        return "R" + diameter + "/" + brandWheel;
     }
 }
