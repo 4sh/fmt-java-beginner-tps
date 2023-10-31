76186cadd5ee7e6a46fadd195f6028f722c9f2b2
Morgan Renou <morgan.renou@4sh.fr>
2023-10-31 20:38:52 +0100

    switch expression and String pool

--- src/main/java/qsh/learning/javaBeginner/vehicle/Wheel.java
+++ src/main/java/qsh/learning/javaBeginner/vehicle/Wheel.java
@@ -1,5 +1,7 @@
 package qsh.learning.javaBeginner.vehicle;
 
+import java.util.Objects;
+
 public record Wheel(String brandWheel, int diameter) {
     public static Integer wheelCounter = 0;
 
@@ -9,6 +11,20 @@ public record Wheel(String brandWheel, int diameter) {
 
     @Override
     public String toString() {
-        return "R" + diameter + "/" + brandWheel.substring(0,5).toUpperCase();
+        return "R" + diameter + "/" + buildBrandTag(brandWheel);
+    }
+
+    private String buildBrandTag(String brandWheel) {
+        return brandWheel.substring(0, 5).toUpperCase();
+    }
+
+    public boolean compatibleWith(Wheel wheel) {
+        var typeA = getType(brandWheel);
+        var typeB = getType(wheel.brandWheel);
+        return typeA == typeB;
+    }
+
+    private String getType(String brandWheel) {
+       return // ...
     }
 }
