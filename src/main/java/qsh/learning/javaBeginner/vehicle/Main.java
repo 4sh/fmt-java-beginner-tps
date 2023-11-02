fce391186c12b3031a2375d4fe2c34eb0bc0ac93
Morgan Renou <morgan.renou@4sh.fr>
2023-11-02 13:42:03 +0100

    array

--- src/main/java/qsh/learning/javaBeginner/vehicle/Main.java
+++ src/main/java/qsh/learning/javaBeginner/vehicle/Main.java
@@ -1,88 +1,44 @@
 package qsh.learning.javaBeginner.vehicle;
 
 import qsh.learning.javaBeginner.vehicle.engined.Car;
+import qsh.learning.javaBeginner.vehicle.engined.Kart;
+import qsh.learning.javaBeginner.vehicle.engined.Solex;
+import qsh.learning.javaBeginner.vehicle.terrestrial.Bike;
 
 public class Main {
     public static void main(String[] args) {
-        var car1 = new Car(
-                new Wheel("Michelin", 19),
-                new Wheel("Michelin", 19),
-                new Wheel("GoodYear", 17),
-                new Wheel("GoodYear pneu", 17),
-                "AZ789BB",
-                50);
-        testWheelOk(car1);
-
-        var car2 = new Car(
-                new Wheel("GoodYear", 19),
-                new Wheel("Michelin", 19),
-                new Wheel("Michelin", 17),
-                new Wheel("GoodYear pneu", 17),
-                "AZ789BB",
-                50);
-        testWheelOk(car2);
-
-        var car3 = new Car(
-                new Wheel("Continental", 19),
-                new Wheel("GoodYear", 19),
-                new Wheel("Continental", 17),
-                new Wheel("GoodYear", 17),
-                "AZ789BB",
-                50);
-        testWheelKo(car3);
+        Wheel wheel1 = new Wheel("Michelin", 19);
+        Wheel wheel2 = new Wheel("Michelin", 16);
+        Wheel wheel3 = new Wheel("GoodYear", 17);
+        Wheel wheel4 = new Wheel("GoodYear pneu", 15);
 
 
-        var car4 = new Car(
-                new Wheel("PirELLI", 19),
-                new Wheel("Continental prenium", 19),
-                new Wheel("PirEli", 17),
-                new Wheel("Continental prenium", 17),
-                "AZ789BB",
-                50);
-        testWheelOk(car4);
+        System.out.println("TEST CAR");
+        var car = new Car(wheel1, wheel2, wheel3, wheel4, "AZ789BB", 50);
+        System.out.println("test RightFrontWheel : " + wheel1.equals(car.getRightFrontWheel()));
+        System.out.println("test LeftFrontWheel : " + wheel2.equals(car.getLeftFrontWheel()));
+        System.out.println("test RightRearWheel : " + wheel3.equals(car.getRightRearWheel()));
+        System.out.println("test LeftRearWheel : " + wheel4.equals(car.getLeftRearWheel()));
+        System.out.println("test nbWheels should be 4 : " + car.getNbOfWheels());
 
-        var car5 = new Car(
-                new Wheel("PirELLI", 19),
-                new Wheel("Dunlop", 19),
-                new Wheel("PirEli", 17),
-                new Wheel("Dunlop", 17),
-                "AZ789BB",
-                50);
-        testWheelKo(car5);
+        System.out.println("TEST SOLEX");
+        var solex = new Solex(wheel1, wheel2, 50);
+        System.out.println("test FrontWheel : " + wheel1.equals(solex.getFrontWheel()));
+        System.out.println("test RearWheel : " + wheel2.equals(solex.getRearWheel()));
+        System.out.println("test nbWheels should be 2 : " + solex.getNbOfWheels());
 
-        var car6 = new Car(
-                new Wheel("Tristar", 19),
-                new Wheel("Dunlop", 19),
-                new Wheel("Tristar", 17),
-                new Wheel("Dunlop", 17),
-                "AZ789BB",
-                50);
-        testWheelKo(car6);
+        System.out.println("TEST KART");
+        var kart = new Kart(wheel1, wheel2, wheel3, wheel4, 50);
+        System.out.println("test RightFrontWheel : " + wheel1.equals(kart.getRightFrontWheel()));
+        System.out.println("test LeftFrontWheel : " + wheel2.equals(kart.getLeftFrontWheel()));
+        System.out.println("test RightRearWheel : " + wheel3.equals(kart.getRightRearWheel()));
+        System.out.println("test LeftRearWheel : " + wheel4.equals(kart.getLeftRearWheel()));
+        System.out.println("test nbWheels should be 4 : " + kart.getNbOfWheels());
 
-        System.out.println("Wheel prepared : " + Wheel.wheelCounter);
-    }
-
-    private static void testWheelOk(Car car) {
-        System.out.println(car.getWheelsDescription());
-        if (!car.getRightFrontWheel().compatibleWith(car.getLeftFrontWheel()) ||
-                !car.getRightRearWheel().compatibleWith(car.getLeftRearWheel())) {
-            System.out.println("FAIL: TEST WHEEL KO, should be OK ");
-        } else {
-            System.out.println("SUCCESS :TEST WHEEL OK");
-
-        }
-        System.out.println("---");
-    }
-
-    private static void testWheelKo(Car car) {
-        System.out.println(car.getWheelsDescription());
-        if (car.getRightFrontWheel().compatibleWith(car.getLeftFrontWheel()) &&
-                car.getRightRearWheel().compatibleWith(car.getLeftRearWheel())) {
-            System.out.println("FAIL: TEST WHEEL OK, should be KO ");
-        } else {
-            System.out.println("SUCCESS :TEST WHEEL KO");
-
-        }
-        System.out.println("---");
+        System.out.println("TEST BIKE");
+        var bike = new Bike(wheel1, wheel2);
+        System.out.println("test FrontWheel : " + wheel1.equals(bike.getFrontWheel()));
+        System.out.println("test RearWheel : " + wheel2.equals(bike.getRearWheel()));
+        System.out.println("test nbWheels should be 2 : " + bike.getNbOfWheels());
     }
 }
