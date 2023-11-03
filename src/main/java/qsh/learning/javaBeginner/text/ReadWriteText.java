9fd89094fc8c46b74ecbc96afe6a10a4cd96b32a
Morgan Renou <morgan.renou@4sh.fr>
2023-11-03 21:00:29 +0100

    solution : list directory

--- src/main/java/qsh/learning/javaBeginner/text/ReadWriteText.java
+++ src/main/java/qsh/learning/javaBeginner/text/ReadWriteText.java
@@ -1,9 +1,6 @@
 package qsh.learning.javaBeginner.text;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -38,8 +35,9 @@ public class ReadWriteText {
 
 
     public static void main(String[] args) {
-
-
-
+        Arrays.stream(Paths.get(".").toFile().listFiles())
+                .filter(File::isFile)
+                .map(File::getAbsolutePath)
+                .forEach(System.out::println);
     }
 }
