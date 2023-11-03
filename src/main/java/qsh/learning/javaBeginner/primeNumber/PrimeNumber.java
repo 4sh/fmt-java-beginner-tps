b0f87327f2b190907e272dbda948f7ce2be61301
Morgan Renou <morgan.renou@4sh.fr>
2023-11-03 17:55:13 +0100

    solution: prime number with stream api

--- src/main/java/qsh/learning/javaBeginner/primeNumber/PrimeNumber.java
+++ src/main/java/qsh/learning/javaBeginner/primeNumber/PrimeNumber.java
@@ -1,28 +1,14 @@
 package qsh.learning.javaBeginner.primeNumber;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class PrimeNumber {
 
     public static void main(String[] args) {
-        byte nbOfPrimeNumberToFind = 100;
-        byte counter = 0;
-
-        short number = 2; //0, 1 are not prime number by definition.
-
-        while (counter < nbOfPrimeNumberToFind) {
-            boolean isPrimeNumber = true;
-
-            for (short i = 2; i < number; i++) {
-                if (number % i == 0) {
-                    isPrimeNumber = false;
-                }
-            }
-
-            if (isPrimeNumber) {
-                counter++;
-                System.out.println(number);
-            }
-
-            number++;
-        }
+        Stream.iterate(2, i -> i + 1)
+                .filter(i -> IntStream.range(2, i).noneMatch(n -> i % n == 0))
+                .limit(100)
+                .forEach(System.out::println);
     }
 }
