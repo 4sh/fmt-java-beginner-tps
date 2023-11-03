6b5d8b33d0d5e6853a382e87e896589181b246ff
Morgan Renou <morgan.renou@4sh.fr>
2023-11-03 17:56:08 +0100

    solution: stream api into TerrestrialVehicle

--- src/main/java/qsh/learning/javaBeginner/vehicle/terrestrial/TerrestrialVehicle.java
+++ src/main/java/qsh/learning/javaBeginner/vehicle/terrestrial/TerrestrialVehicle.java
@@ -1,5 +1,6 @@
 package qsh.learning.javaBeginner.vehicle.terrestrial;
 
+import qsh.learning.javaBeginner.DistinctByKey;
 import qsh.learning.javaBeginner.vehicle.NoAccelerationException;
 import qsh.learning.javaBeginner.vehicle.TooHighSpeedException;
 import qsh.learning.javaBeginner.vehicle.Vehicle;
@@ -8,12 +9,15 @@ import qsh.learning.javaBeginner.vehicle.wheel.Wheel;
 import qsh.learning.javaBeginner.vehicle.wheel.WheelId;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static qsh.learning.javaBeginner.DistinctByKey.uniqueBy;
 
 public class TerrestrialVehicle implements Vehicle {
     private final boolean oneWheelsLine;
     protected List<Wheel> rightWheels = new ArrayList<>();
     protected List<Wheel> leftWheels = new ArrayList<>();
-    private final Map<WheelId, Wheel> allWheels = new HashMap<>();
+    private final Map<WheelId, Wheel> allWheels;
     private int speed = 0;
 
     public TerrestrialVehicle(List<Wheel> wheels) {
@@ -22,32 +26,13 @@ public class TerrestrialVehicle implements Vehicle {
 
     public TerrestrialVehicle(List<Wheel> wheels, boolean oneWheelsLine) {
         this.oneWheelsLine = oneWheelsLine;
-        setWheels(wheels);
-        sortWheels();
-    }
-
-    private void setWheels(List<Wheel> wheels) {
-        for (var wheel : wheels) {
-            if (!allWheels.containsKey(wheel.wheelId())) {
-                allWheels.put(wheel.wheelId(),wheel);
-                if (wheel.direction() == DirectionWheel.RIGHT && !oneWheelsLine) {
-                    this.rightWheels.add(wheel);
-                } else {
-                    this.leftWheels.add(wheel);
-                }
-            }
-        }
-    }
-
-    private void sortWheels() {
-        var comparator = new Comparator<Wheel>() {
-            @Override
-            public int compare(Wheel o1, Wheel o2) {
-                return Double.compare(o1.wear(), o2.wear());
-            }
-        }.reversed();
-        this.rightWheels.sort(comparator);
-        this.leftWheels.sort(comparator);
+        allWheels = wheels.stream().collect(Collectors.toMap(Wheel::wheelId, wheel -> wheel));
+        Map<Boolean, List<Wheel>> collect = wheels.stream()
+                .sorted(Comparator.comparingDouble(Wheel::wear).reversed())
+                .filter(uniqueBy(Wheel::wheelId))
+                .collect(Collectors.partitioningBy(wheel -> wheel.direction() == DirectionWheel.RIGHT && !oneWheelsLine));
+        this.rightWheels = collect.get(true);
+        this.leftWheels = collect.get(false);
     }
 
     public void accelerate() throws NoAccelerationException, TooHighSpeedException {
