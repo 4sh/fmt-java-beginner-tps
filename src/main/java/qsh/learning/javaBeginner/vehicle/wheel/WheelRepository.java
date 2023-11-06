0f300633b288f5d33d21aeb883f7e2b03dd9dfbe
Morgan Renou <morgan.renou@4sh.fr>
2023-11-06 17:37:18 +0100

    solution: jdbc

--- src/main/java/qsh/learning/javaBeginner/vehicle/wheel/WheelRepository.java
+++ src/main/java/qsh/learning/javaBeginner/vehicle/wheel/WheelRepository.java
@@ -24,18 +24,58 @@ public class WheelRepository {
     public WheelRepository(Connection conn) {
         this.conn = conn;
         initSchema();
-        // initialiser les PreparedStatement
+        try {
+            insertWheelStatement = conn.prepareStatement("INSERT INTO WHEEL VALUES (?, ?, ?, ?, ?)");
+            findByBrandStatement = conn.prepareStatement("SELECT * FROM WHEEL WHERE brandWheel = ?");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void initSchema() {
-        // utiliser des Statment pour les requête DLL
+        try (Statement statement = conn.createStatement()) {
+            statement.executeUpdate(DROP_TABLE);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try (Statement statement = conn.createStatement()) {
+            statement.executeUpdate(CREATE_TABLE);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void insertWheel(Wheel wheel) {
-        //
+        try {
+            insertWheelStatement.setString(1, wheel.wheelId().getBrandWheel().name());
+            insertWheelStatement.setString(2, wheel.wheelId().getSerialId());
+            insertWheelStatement.setInt(3, wheel.diameter());
+            insertWheelStatement.setString(4, wheel.direction().name());
+            insertWheelStatement.setDouble(5, wheel.wear());
+            insertWheelStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Wheel> findWheelByBrand(BrandWheel byBrandWheel) {
-      //
+        try {
+            findByBrandStatement.setString(1, byBrandWheel.name());
+            ResultSet resultSet = findByBrandStatement.executeQuery();
+
+            List<Wheel> result = new ArrayList<>();
+            while (resultSet.next()) {
+                String brandWheel = resultSet.getString("brandWheel");
+                String serialId = resultSet.getString("serialId");
+                int diameter = resultSet.getInt("diameter");
+                DirectionWheel direction = DirectionWheel.valueOf(resultSet.getString("direction"));
+                double wear = resultSet.getDouble("wear");
+                Wheel wheel = new Wheel(new WheelId(brandWheel, serialId), diameter, direction, wear);
+                result.add(wheel);
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
